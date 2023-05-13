7c340b2fc820b849a267ba81266262b232d1ad84
kyohei_i <kyoheishogun.1997@i.softbank.jp>
2023-05-13 12:24:30 +0900

    PostalCodeREpositoryのキャッシュ機能を追加

--- postal_code_db/postal_code_repository.py
+++ postal_code_db/postal_code_repository.py
@@ -1,19 +1,27 @@
 import sqlite3
 
+from functools import lru_cache
+
 
 class PostalCodeRepository(object):
+    def __init__(self):
+        self.postal_codes = {}
 
+    @lru_cache(maxsize=128)
     def fetch_address(self, code: str) -> str:
-        conn = sqlite3.connect('postal_code_db/postal_code.db')
-        curs = conn.cursor()
-        curs.execute(
-            'SELECT address FROM "postal_code" WHERE code = ?;',
-            (code,)
-        )
-        tuple_address = curs.fetchone()
-        curs.close()
-        conn.close()
-        if tuple_address is None:
-            return None
-        address, = tuple_address
-        return address
+        if code not in self.postal_codes:
+            conn = sqlite3.connect('postal_code_db/postal_code.db')
+            curs = conn.cursor()
+            curs.execute(
+                'SELECT address FROM "postal_code" WHERE code = ?;',
+                (code,)
+            )
+            tuple_address = curs.fetchone()
+            curs.close()
+            conn.close()
+            if tuple_address is None:
+                return None
+            address, = tuple_address
+            self.postal_codes[code] = address
+            return address
+        return self.postal_codes[code]
